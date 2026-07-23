426105732da0b62ce6ad17d0562a7cf302b70f08
Era1401 <eratodkar@gmail.com>
2026-07-23 07:32:07 +0000

    Updated HashMapImp

--- HashMapImp.java
+++ HashMapImp.java
@@ -8,14 +8,19 @@ public class HashMapImp{
         hm.put(3, "France");
         hm.put(4, "England");
         hm.putifAbsent(5, "Portugal");
+        
         hm.remove(3);
+
         System.out.println("Item at key 2: " + hm.get(2));
         System.out.println("Hashmap size: " + hm.size());
         System.out.println("HashMap is empty: " + hm.isEmpty());
+
         System.out.println("\n" + "Keys: ");
         hm.forEach((key, value) -> System.out.print(key + " "));
+
         System.out.println("\n" + "Values: ");
         hm.forEach((key, value) -> System.out.print(value + " "));
+
         System.out.println("\n");
         hm.forEach((key, value) -> System.out.println(key + " : " + value));
 
