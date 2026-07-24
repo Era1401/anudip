9d8040e3f8d312d8902ca13384a7b0b387bd738a
Era1401 <eratodkar@gmail.com>
2026-07-24 05:18:11 +0000

    Added ThreadEx1

---
+++ ThreadEx1.java
@@ -0,0 +1,17 @@
+public class ThreadEx1 implements Runnable {
+    @Override
+    public void run(){
+        int i = 1;
+        for(; ; ){
+            System.out.println("World" + i);
+            i++;
+        }
+    }
+
+    public static void main(String[] args){
+        ThreadEx1 obj = new ThreadEx1();
+        Thread obj1 = new Thread(obj);
+        obj1.start();
+
+    }
+}
