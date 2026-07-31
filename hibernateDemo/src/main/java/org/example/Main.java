3bb5784edaeb2e9091c924c9337aa464b8336d15
Era1401 <eratodkar@gmail.com>
2026-07-31 15:08:10 +0530

    Updated Main method with other CRUD operations
    Added functionality to insert, update, and delete students in the database.

--- hibernateDemo/src/main/java/org/example/Main.java
+++ hibernateDemo/src/main/java/org/example/Main.java
@@ -11,13 +11,43 @@ public class Main {
         Configuration config = new Configuration();
         config.configure("hibernate.cfg.xml");
         SessionFactory factory = config.buildSessionFactory();
+
+        //Insert
         Session session1 = factory.openSession();
         Transaction transaction1 = session1.beginTransaction();
+
         Student obj = new Student(1,"Era",98);
+        Student obj1 = new Student(2, "Trishala", 100);
         session1.persist(obj);
+        session1.persist(obj1);
         transaction1.commit();
-        session1.close();
-        factory.close();
         System.out.println("Student saved successfully");
+
+        //Update
+        Session session2 = factory.openSession();
+        Transaction transaction2 = session2.beginTransaction();
+        Student student = session2.get(Student.class, 1);
+        if(student != null){
+            student.setName("Era Ajay Todkar");
+            student.setMarks(99);
+        }
+        transaction2.commit();
+        System.out.println("Student updated successfully");
+
+        //Delete
+        Session session3 = factory.openSession();
+        Transaction transaction3 = session3.beginTransaction();
+        student = session3.get(Student.class, 1);
+        session3.remove(student);
+        transaction3.commit();
+        System.out.println("Student deleted successfully");
+
+
+        session1.close();
+        session2.close();
+        session3.close();
+
+        factory.close();
+
     }
 }
