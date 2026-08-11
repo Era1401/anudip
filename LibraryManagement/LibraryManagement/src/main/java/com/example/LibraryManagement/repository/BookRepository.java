a759076c85e455dc740afba580b90223cbddcf21
Era1401 <eratodkar@gmail.com>
2026-08-11 13:07:47 +0530

    Enhance BookRepository
    Added methods to get, save, update, and delete books by ID.

--- LibraryManagement/LibraryManagement/src/main/java/com/example/LibraryManagement/repository/BookRepository.java
+++ LibraryManagement/LibraryManagement/src/main/java/com/example/LibraryManagement/repository/BookRepository.java
@@ -10,6 +10,8 @@ import java.util.List;
 public class BookRepository {
 
     private final List<Book> books = new ArrayList<>();
+    private Integer nextId = 3;
+
     public BookRepository(){
         books.add(
                 new Book(1, "Java Basics", "James Gosling", 500));
@@ -19,6 +21,48 @@ public class BookRepository {
 
     public List<Book> findAll(){
         return books;
+    }
+
+    //Find the data by id
+    public Book getBookById(int id){
+        for(Book book : books){
+            if(book.getId() == id){
+                return book;
+            }
+        }
+        return null;
+    }
+
+    //Post Mapping: Add the data
+    public Book save(Book book){
+        book.setId(nextId++);
+        books.add(book);
+        return book;
+
+    }
+
+    //Put Mapping: Update the Data
+    public Book update(Integer id, Book updateBook){
+        for(Book book : books){
+            if(book.getId() == id ){
+                book.setTitle(updateBook.getTitle());
+                book.setAuthor(updateBook.getAuthor());
+                book.setPrice(updateBook.getPrice());
+                return book;
+            }
+        }
+        return null;
+    }
+
+    //Delete Mapping: Delete the data
+    public boolean deleteById(Integer id){
+        for(Book book: books){
+            if(book.getId() == id){
+                books.remove(book);
+                return true;
+            }
+        }
+        return false;
     }
 
 }
